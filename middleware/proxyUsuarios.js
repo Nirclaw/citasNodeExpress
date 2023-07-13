fdf7f7336a9c9a824ea9388274c7edbe36df25c1
Nirclaw <caicedonicolas150@gmail.com>
2023-07-12 22:39:27 -0500

    feat: :sparkles: se crea el middleware del la entrada de datos de usuarios para poder gestionar correctamente los datos y mayor se guridad

---
+++ middleware/proxyUsuarios.js
@@ -0,0 +1,21 @@
+import express from "express";
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+import { user } from "../controller/user.js";
+
+
+const proxyUser = express();
+
+proxyUser.use((req,res,next)=>{
+    try {
+        let data = plainToClass(user, req.body,{excludeExtraneousValues:true})
+        
+        req.body = JSON.parse(JSON.stringify(data));
+        
+        next();
+    } catch (err) {
+        res.status(err.status).send(err)
+    }
+})
+
+export default proxyUser;
